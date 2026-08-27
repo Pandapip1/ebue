6eac59bd7416223a0031ec7aab3b7c55bea76525
Gavin John <gjohn@rtr.ai>
2026-08-27 19:46:55 -0400

    pthread: expose implemented rwlock entry points

--- include/pthread.h
+++ include/pthread.h
@@ -166,6 +166,20 @@ int pthread_condattr_setclock(pthread_condattr_t *, clockid_t);
 int pthread_condattr_getpshared(const pthread_condattr_t *__restrict, int *__restrict);
 int pthread_condattr_setpshared(pthread_condattr_t *, int);
 
+int pthread_rwlock_init(pthread_rwlock_t *__restrict, const pthread_rwlockattr_t *__restrict);
+int pthread_rwlock_destroy(pthread_rwlock_t *);
+int pthread_rwlock_rdlock(pthread_rwlock_t *);
+int pthread_rwlock_tryrdlock(pthread_rwlock_t *);
+int pthread_rwlock_timedrdlock(pthread_rwlock_t *__restrict, const struct timespec *__restrict);
+int pthread_rwlock_wrlock(pthread_rwlock_t *);
+int pthread_rwlock_trywrlock(pthread_rwlock_t *);
+int pthread_rwlock_timedwrlock(pthread_rwlock_t *__restrict, const struct timespec *__restrict);
+int pthread_rwlock_unlock(pthread_rwlock_t *);
+int pthread_rwlockattr_init(pthread_rwlockattr_t *);
+int pthread_rwlockattr_destroy(pthread_rwlockattr_t *);
+int pthread_rwlockattr_getpshared(const pthread_rwlockattr_t *__restrict, int *__restrict);
+int pthread_rwlockattr_setpshared(pthread_rwlockattr_t *, int);
+
 int pthread_getschedparam(pthread_t, int *__restrict, struct sched_param *__restrict);
 int pthread_setschedparam(pthread_t, int, const struct sched_param *);
 int pthread_setschedprio(pthread_t, int);
