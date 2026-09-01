71f03ed6b6e54fa4fea239eccd9191d7ce7f9d53
Gavin John <gjohn@rtr.ai>
2026-09-01 19:56:32 -0400

    Automate direct writable span contracts

--- src/string/stpncpy.c
+++ src/string/stpncpy.c
@@ -7,7 +7,8 @@
 // NOLINTBEGIN(misc-include-cleaner)
 #include <string.h>
 
-char *stpncpy(char *__restrict d, const char *__restrict s, size_t n)
+char *stpncpy(char *__restrict d withtok(writable_span(n)),
+	const char *__restrict s, size_t n)
 {
 	for (; n && (*d = *s); n--, s++, d++);
 	memset(d, 0, n);
