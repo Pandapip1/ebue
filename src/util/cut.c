fb1b40f1c6b8aa24340ef07a6ddf1e35f958ba07
Gavin John <gjohn@rtr.ai>
2026-09-01 15:58:38 -0400

    index cut fields without pointer differences

--- src/util/cut.c
+++ src/util/cut.c
@@ -196,21 +196,21 @@ static void process_field_mode(FILE *f, const struct range *ranges, size_t nr, c
 
 		{
 			long field = 1;
-			char *start = line;
-			char *end = line + ulen;
+			size_t start = 0;
 			int wrote_any = 0;
 
 			for (;;) {
-				char *p = memchr(start, delim, (size_t)(end - start));
-				size_t flen = p ? (size_t)(p - start) : (size_t)(end - start);
+				size_t end = start;
+
+				while (end < ulen && line[end] != delim) end++;
 
 				if (is_selected(ranges, nr, field)) {
 					if (wrote_any) fputc(delim, stdout);
-					fwrite(start, 1, flen, stdout);
+					fwrite(line + start, 1, end - start, stdout);
 					wrote_any = 1;
 				}
-				if (!p) break;
-				start = p + 1;
+				if (end == ulen) break;
+				start = end + 1;
 				field++;
 			}
 			if (had_nl) fputc('\n', stdout);
