8e338848f1f967100e2394ab9c99dfe864e1bbb5
Gavin John <gjohn@rtr.ai>
2026-09-01 15:59:25 -0400

    bound cp write retries

--- src/util/cp.c
+++ src/util/cp.c
@@ -112,9 +112,12 @@ int __util_copy_regular_file(const char *src, const char *dst, int force)
 
 	while ((n = read(in, buf, sizeof buf)) > 0) {
 		char *p = buf;
-		while (n > 0) {
+		ssize_t remaining = n;
+		while (n > 0 && remaining > 0) {
 			ssize_t w = write(out, p, (size_t)n);
-			if (w < 0) {
+			remaining--;
+			if (w <= 0 || w > n) {
+				if (w >= 0) errno = EIO;
 				__util_diagf("cp: error writing to '%s': %s\n", dst, strerror(errno));
 				(void)close(in);
 				(void)close(out);
